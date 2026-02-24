84e55633c3d406cffdb647e6623ae7794bc795c7
Asmaa Osama <ao9191360@gmail.com>
2026-02-24 22:05:25 +0200

    Update front-end attributes and API connection

--- ExamCorrection/Persistance/Migrations/20260224195818_UpdateTotalQuestionsToFloat.Designer.cs
+++ ExamCorrection/Persistance/Migrations/20260224195818_UpdateTotalQuestionsToFloat.Designer.cs
@@ -4,6 +4,7 @@ using ExamCorrection.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExamCorrection.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260224195818_UpdateTotalQuestionsToFloat")]
+    partial class UpdateTotalQuestionsToFloat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ExamCorrection/Persistance/Migrations/20260224195818_UpdateTotalQuestionsToFloat.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExamCorrection.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTotalQuestionsToFloat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "TotalQuestions",
+                table: "StudentExamPapers",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "X",
+                table: "ExamPages",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Y",
+                table: "ExamPages",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "X",
+                table: "ExamPages");
+
+            migrationBuilder.DropColumn(
+                name: "Y",
+                table: "ExamPages");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalQuestions",
+                table: "StudentExamPapers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+        }
+    }
+}
